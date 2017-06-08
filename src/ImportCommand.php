096978a1322495d06fbda9656f8a7b170574d033
Niko Sams <ns@vivid-planet.com>
2017-06-08 15:59:48 +0200

    initial import

---
+++ src/ImportCommand.php
@@ -0,0 +1,19 @@
+<?php
+namespace ComponentContentImportExport;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ImportCommand extends Command
+{
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // ...
+    }
+}
