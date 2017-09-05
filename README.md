45230797158f883bc61327d5237e7f5b753a8a7f
Matthias Maus <mm@vivid-planet.com>
2017-09-05 09:35:22 +0200

    added the option to use a different file for references on convert:from-po
    edited readme accordingly

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
     ./vendor/bin/component-content-import-export export --component-id=root | ./vendor/bin/component-content-import-export convert:to-po > out.po
     ./vendor/bin/component-content-import-export convert:from-po < out.po | ./vendor/bin/component-content-import-export import
     
+    When converting from PO to the importable format --reference-file=fileName or -r filename can be used to provide a different file for references. When a msgid doesnt exist in the referencefile it gets skipped.
+    If no file is provided, input-file is used for references.
+    
     # Xliff
     ./vendor/bin/component-content-import-export export --component-id=root | ./vendor/bin/component-content-import-export convert:to-xliff -l de-DE > out.xlf
     ./vendor/bin/component-content-import-export convert:from-xliff < out.xlf | ./vendor/bin/component-content-import-export import
     
     On convert:to-xliff "-l" or "--source-lang" can be used to define the source-language attribute for the xliff-document. Without the standard "de-AT" is used.
     
+
--- src/ConvertFromPoCommand.php
+++ src/ConvertFromPoCommand.php
@@ -2,9 +2,12 @@
 namespace ComponentContentImportExport;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
+
 use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+
 use Geekwright\Po\PoHeader;
 use Geekwright\Po\PoTokens;
 use Geekwright\Po\PoFile;
@@ -16,6 +19,8 @@ class ConvertFromPoCommand extends ConvertAbstractCommand
     {
         $this->setName('convert:from-po');
         $this->setDescription('Converts a exported document to the po format.');
+        $this->addOption('reference-file', 'r', InputOption::VALUE_REQUIRED,
+            'Use a different file for references (e.g. new Export). If no file is provided, input-file is used for references');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -23,12 +28,30 @@ class ConvertFromPoCommand extends ConvertAbstractCommand
         $errOutput = $output instanceof ConsoleOutputInterface ? $output->getErrorOutput() : $output;
         $contents = $this->readInput($input, $output);
 
+        $referenceFileName = $input->getOption('reference-file');
+        $referenceFile = null;
+        if ($referenceFileName) {
+            $referenceFile = new Pofile();
+            $referenceFile->parsePoSource(file_get_contents($referenceFileName));
+        }
         $poFile = new PoFile();
         $poFile->parsePoSource($contents);
         foreach ($poFile->getEntries() as $entry) {
             $message = $entry->getAsString(PoTokens::MESSAGE);
             $translated = $entry->getAsString(PoTokens::TRANSLATED);
-            $references = explode(' ', implode(' ', $entry->getAsStringArray(PoTokens::REFERENCE)));
+
+            if ($referenceFileName) {
+                $poReferences = $referenceFile->findEntry($entry->getAsString(PoTokens::MESSAGE));
+                if (!empty($poReferences)) {
+                    $poReferences = $poReferences->getAsStringArray(PoTokens::REFERENCE);
+                } else {
+                    $errOutput->writeln('msgid "'.$entry->getAsString(PoTokens::MESSAGE).'" was not found in reference file, skipped'.PHP_EOL);
+                    continue;
+                }
+            } else {
+                $poReferences = $entry->getAsStringArray(PoTokens::REFERENCE);
+            }
+            $references = explode(' ', implode(' ', $poReferences));
             if (!$references) {
                 $errOutput->writeln("<error>missing reference for $message</error>");
             }
